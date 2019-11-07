d32fa41efafa26cefc63fcac3523bf21303cd201
David Asaf <asaf.david.e@gmail.com>
2019-11-07 13:24:54 -0500

    CH. 5 flask-accepts

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ from logging import Logger
 from typing import List
 
 from flask import Flask, abort, jsonify, request
+from flask_accepts import responds, accepts
 from flask_praetorian import Praetorian, auth_required, roles_required
 from flask_restplus import Api, Resource, Namespace, fields
 from flask_sqlalchemy import SQLAlchemy
 
-from app.schemas.user import UserSchema, UserSchemaWithPassword
+from app.schemas.user import UserSchema, UserSchemaWithPassword, UserLoginSchema
 
 db = SQLAlchemy()
 guard = Praetorian()
@@ -62,22 +63,21 @@ def create_app(config_name: str) -> Flask:
     #
 
     # Attach routes to namespace
-    @api.route("/login")
-    @api.doc(
-        body=api.model("Login", {"username": fields.String, "password": fields.String})
-    )
-    class UserLoginResource(Resource):
-        def post(self):
-            # Ignore the mimetype and always try to parse JSON.
-            req = request.get_json(force=True)
 
-            username = req.get("username", None)
-            password = req.get("password", None)
+    @api.route("/login")
+    class UserLoginResource(Resource):
+        @accepts(schema=UserLoginSchema, api=api)
+        @responds(dict(name="access_token", type=str), status_code=200, api=api)
+        def post(self):
+            # I can confidently access parsed_args based on @accepts criteria
+            # use request.parsed_obj for body
+            # use request.parsed_args for query params
+            username = request.parsed_obj["username"]
+            password = request.parsed_obj["password"]
 
             user = guard.authenticate(username, password)
             ret = {"access_token": guard.encode_jwt_token(user)}
-
-            return jsonify(ret)
+            return ret
 
     @user_api.route("")
     @user_api.doc(security="jwt")
